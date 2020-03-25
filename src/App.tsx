13c5d537d23a359d6e7899c8da13a700d2ff8707
izaakrogan <izaakrogan@googlemail.com>
2020-03-25 14:36:03 +0000

    adds hot reloading

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import styled from 'styled-components';
+import { hot } from 'react-hot-loader/root';
 
 import './App.css';
 import UserInputsPanel from './components/UserInputsPanel';
 import Visualisations from './components/Visualisations';
 
-export default function App() {
+const App = () => {
   return (
     <Container>
       <UserInputsPanel />
       <Visualisations />
     </Container>
   );
-}
+};
+
+export default hot(App);
 
 const Container = styled.div`
   height: 100vh;
