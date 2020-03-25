950637dc1a5a647b31870edf13ce1270e3a28092
eadehemingway <eade.hemingway@gmail.com>
2020-03-25 13:32:26 +0000

    make force directed work in one svg

--- src/components/ForceDirected.tsx
+++ src/components/ForceDirected.tsx
@@ -4,26 +4,23 @@ import * as d3 from 'd3';
 
 interface Props {
   deathRate: number;
-  id: string;
+  position: number;
 }
 
-export default function ForceDirected({ deathRate, id }: Props) {
+export default function ForceDirected({ deathRate, position }: Props) {
   const [data, setData] = useState([]);
 
   useEffect(() => {
     const data = d3.range(100).map((n, i) => {
       const isDeathCircle = n >= deathRate;
-      const color = isDeathCircle ? 'orange' : 'brown';
-      return { id: i, num: n, color };
+      const fillColor = isDeathCircle ? 'none' : '#C4C4C4';
+      return { id: i, num: n, fillColor };
     });
-    d3.select(`#${id}`)
-      .attr('width', 200)
-      .attr('height', 600);
     setData(data);
-  }, [deathRate, id]);
+  }, [deathRate]);
 
   useEffect(() => {
-    const centerOfGravity = { x: 100, y: 300 };
+    const centerOfGravity = { x: 200 * position, y: 300 };
     const forceX = d3
       .forceX()
       .x(centerOfGravity.x)
@@ -48,34 +45,25 @@ export default function ForceDirected({ deathRate, id }: Props) {
       .alphaDecay(0)
       .on('tick', () => {
         // call the tick function running the simulation
-        d3.selectAll(`.circle-${id}`).attr(
+        d3.selectAll(`.circle-${position}`).attr(
           'transform',
           (d: dataWithCoordinates) => `translate(${d.x} ${d.y})`
         );
       });
 
-    const svg = d3.select(`#${id}`);
+    const svg = d3.select('#visualisations');
 
     svg
-      .selectAll(`.circle-${id}`)
+      .selectAll(`.circle-${position}`)
       .data(data)
       .enter()
       .append('circle')
       .attr('r', radius)
-      .attr('class', `circle-${id}`)
-      .attr('stroke', d => d.color)
+      .attr('class', `circle-${position}`)
+      .attr('stroke', '#FE9839')
       .attr('stroke-width', 2)
-      .attr('fill', 'none');
-  }, [data, id]);
+      .attr('fill', d => d.fillColor);
+  }, [data, position]);
 
-  return (
-    <Container>
-      <svg id={id} />
-    </Container>
-  );
+  return <div></div>;
 }
-
-const Container = styled.div`
-  display: flex;
-  flex: 2;
-`;
--- src/components/Visualisations.tsx
+++ src/components/Visualisations.tsx
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styled from 'styled-components';
 import ForceDirected from './ForceDirected';
+import * as d3 from 'd3';
 
-export default function Drawer() {
+export default function Visualisations() {
+  useEffect(() => {
+    d3.select(`#visualisations`)
+      .attr('width', 800)
+      .attr('height', 600);
+  }, []);
   return (
     <Container>
-      <ForceDirected deathRate={5} id="one" />
-      <ForceDirected deathRate={5} id="two" />
-      <ForceDirected deathRate={5} id="three" />
+      <ForceDirected deathRate={5} position={1} />
+      <ForceDirected deathRate={15} position={2} />
+      <ForceDirected deathRate={25} position={3} />
+      <svg id="visualisations" />
     </Container>
   );
 }
