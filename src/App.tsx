8bea8aae6dae3c84ecca756517198e7508ab2520
izaakrogan <izaakrogan@googlemail.com>
2020-03-25 10:00:01 +0000

    adds basic layout and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
 
-function App() {
+import './App.css';
+import UserInputsDrawer from './components/Drawer';
+import Visualisations from './components/Visualisations';
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <UserInputsDrawer />
+      <Visualisations />
+    </Container>
   );
 }
 
-export default App;
+const Container = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+`;
